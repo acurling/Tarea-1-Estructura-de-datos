5ebb7278d2b7564f3a9935c0d80bd2b7e67f7a18
acurling <acurling@hotmail.com>
2023-09-26 18:37:50 -0600

    actualizacion del programa de java

--- src/Tarea1/Vectores.java
+++ src/Tarea1/Vectores.java
@@ -1,5 +1,6 @@
 
 package Tarea1;
+
 import java.util.Collections;
 import javax.swing.JOptionPane;
 import java.util.Vector;
@@ -20,17 +21,18 @@ public class Vectores {
                     "Digite un numero"));
 
             valores[x] = numero;
-        }//Fin del for
+        } // Fin del for
         mensaje = "";
         for (int i = 0; i < 5; i++) {
             mensaje += "El valor del vector[" + i + "] =" + valores[i] + "\n";
-        }//Fin del for
+        } // Fin del for
 
         JOptionPane.showMessageDialog(null, mensaje);
-    }//Fin del metodo para mostrar el orden original y llenar vector
+    }// Fin del metodo para mostrar el orden original y llenar vector
 
     public void Definir_Vector2() {
         valores1 = new int[5];
+        int x = 0;
 
         for (int x = 0; x < 5; x++) {
             int numero = Integer.parseInt(JOptionPane.showInputDialog(
@@ -41,19 +43,20 @@ public class Vectores {
         mensaje = "";
         for (int i = 0; i < 5; i++) {
             mensaje += "El valor del vector[" + i + "] =" + valores1[i] + "\n";
-        }//Fin del for
+        } // Fin del for
 
         JOptionPane.showMessageDialog(null, mensaje);
-    }//Fin del metodo para mostrar el orden original y llenar vector2
-public void juntarmatrices(){
-int[] combinedArray = Arrays.copyOf(valores, valores.length + valores1.length);
+    }// Fin del metodo para mostrar el orden original y llenar vector2
 
-System.arraycopy(valores, 0, combinedArray, 0, valores.length);
-System.arraycopy(valores1, 0, combinedArray, valores1.length, valores1.length);
+    public void juntarmatrices() {
+        int[] combinedArray = Arrays.copyOf(valores, valores.length + valores1.length);
 
-Arrays.sort(combinedArray);
-JOptionPane.showMessageDialog(null, Arrays.toString(combinedArray));
+        System.arraycopy(valores, 0, combinedArray, 0, valores.length);
+        System.arraycopy(valores1, 0, combinedArray, valores1.length, valores1.length);
 
-}//fin del metodo unir matrices 
+        Arrays.sort(combinedArray);
+        JOptionPane.showMessageDialog(null, Arrays.toString(combinedArray));
 
-}//fin de la clase
+    }// fin del metodo unir matrices
+
+}// fin de la clase
